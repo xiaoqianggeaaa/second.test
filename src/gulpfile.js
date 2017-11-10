1f99a8b05cc9e3b36ff3f16cd645c080a80d12b6
Xiaoqiange <email@example.com>
2017-11-10 19:07:56 +0800

    新的提交

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -16,5 +16,25 @@ gulp.task('create',function(){
 gulp.task('thewatch',function(){
     //监听scss里面的scss文件，如果有修改则执行create任务
     gulp.watch('./sass/*.scss',['create'])
-})
+});
+
+//合并文件
+var concat=require('gulp-concat');
+var uglify=require('gulp-uglify');
+var rename=require('gulp-rename');
+var c_obj={
+    newLine : ";"
+}
+
+gulp.task('isconcat',function(){
+    //引用js文件
+    gulp.src('./js/*.js')
+    .pipe(concat('./js/index.js',c_obj))
+        .pipe(rename({suffix:'.min'}))
+
+        .pipe(gulp.dest('./css/'))
+
+
+        
+});
 
